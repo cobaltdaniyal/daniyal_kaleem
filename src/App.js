5375164a4eb3d19d24c0a64a1af66b0324bae0b6
Daniyal Kaleem <cobaltdaniyal@outlook.com>
2021-05-24 22:56:56 +0500

    daniyal kaleem portfolio

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Daniyal Kaleem
+          Daniyal Kaleem Portfolio
         </p>
       </header>
     </div>
